79277aa526ae23a207d25f4ec0b0b1e804934f5f
oneraul <oneraul@gmail.com>
2017-01-23 04:01:39 +0100

    Add Bullet class. Integrate Bullet into Field. Add internal position to Field and Bullet. Put clicked() code into each class method. Clean up

--- window.lua
+++ window.lua
@@ -1,14 +1,10 @@
 local Window = class("Window", {
+	titleHeight = 16,
 	x = 100, y = 100,
 	width = 200, height = 100,
 	title = "Window",
 	fields = {}
 })
-
-local titleHeight = 16
-local windowMarginX = 15
-local windowFieldHeight = 22
-local windowFieldBulletRadius = 5
 
 function Window:init(x, y, width, height)
 	self.x, self.y = x, y
@@ -22,25 +18,15 @@ function Window:draw()
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
 	love.graphics.printf(self.title, self.x, self.y, self.width, "center")
-	love.graphics.line(self.x, self.y+titleHeight, self.x+self.width, self.y+titleHeight)
+	love.graphics.line(self.x, self.y+self.titleHeight, self.x+self.width, self.y+self.titleHeight)
 	
-	for k, v in ipairs(self.fields) do
-		--love.graphics.printf(v.name, self.x+windowMarginX, self.y+titleHeight+windowFieldHeight*k, self.width-windowMarginX*2, "left")
-		--if v.input then love.graphics.circle("line", self.x, self.y+titleHeight+windowFieldHeight/2+windowFieldHeight*k, windowFieldBulletRadius) end
-		--if v.output then love.graphics.circle("line", self.x+self.width, self.y+titleHeight+windowFieldHeight/2+windowFieldHeight*k, windowFieldBulletRadius) end
-
-		v:draw()
-	end
-end
-	
-function Window:getFieldY(i)
-	return self.y+titleHeight+windowFieldHeight/2+windowFieldHeight*i
+	for k, v in ipairs(self.fields) do v:draw() end
 end
 
 function Window:clicked(x, y)
 	if x > self.x and x < self.x+self.width
 	and y > self.y and y < self.y+self.height then
-		if y < self.y+titleHeight then
+		if y < self.y+self.titleHeight then
 			dragging = self
 		else
 			for k, v in ipairs(self.fields) do
